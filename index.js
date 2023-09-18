9dcd62eb361ae46fc749df166dec3f7cb87a1acc
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-09-18 13:16:37 +0100

    Added mongoose requirement and connection

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+let mongoose = require('mongoose'); // Import mongoose requirement for DB
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology : true});  // Connect to database
+
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
 
 
 
