40b443664e513108ce993d262a3295f8d359799a
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-09-18 13:35:40 +0100

    Created post api route to create a user and user/id response object - ID based off unique uuid module

--- index.js
+++ index.js
@@ -9,14 +9,23 @@ mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTo
 const bodyParser = require('body-parser'); // Import response body parsing middleware
 app.use(bodyParser.urlencoded({extended: false}));  // use body parser middleware for url encoded info
 
+let uuid =require('uuid'); // Import to generate unique ID's
+
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
+app.post('/api/users', function(req,res) {
+  console.log(req.body);
+  let username = req.body.username;
+  let resObj = { username : username,
+                 _id : uuid.v4()
+  }
+  console.log(resObj)
+})
 
 
 
